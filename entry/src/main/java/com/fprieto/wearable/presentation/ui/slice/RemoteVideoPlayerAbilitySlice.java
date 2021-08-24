5c53cba5f16b01922fb7580e45b8f487c28f2dbd
ferPrieto <f.prieto.moyano@gmail.com>
2021-08-24 12:28:35 +0100

    Added previous and next button with messages

--- entry/src/main/java/com/fprieto/wearable/presentation/ui/slice/RemoteVideoPlayerAbilitySlice.java
+++ entry/src/main/java/com/fprieto/wearable/presentation/ui/slice/RemoteVideoPlayerAbilitySlice.java
@@ -3,7 +3,6 @@ package com.fprieto.wearable.presentation.ui.slice;
 import com.fprieto.wearable.ResourceTable;
 import com.fprieto.wearable.model.DataMessage;
 import com.fprieto.wearable.model.PlayerCommand;
-import com.fprieto.wearable.presentation.ui.slice.joke.JokeAbilitySlice;
 import com.fprieto.wearable.util.LogUtils;
 import com.google.gson.Gson;
 import com.huawei.watch.kit.hiwear.HiWear;
@@ -13,17 +12,8 @@ import com.huawei.watch.kit.hiwear.p2p.Receiver;
 import com.huawei.watch.kit.hiwear.p2p.SendCallback;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
-import ohos.agp.components.*;
-import ohos.agp.components.Component.RotationEventListener;
-import ohos.app.dispatcher.TaskDispatcher;
-import ohos.media.image.ImageSource;
-import ohos.media.image.PixelMap;
-import ohos.multimodalinput.event.RotationEvent;
-import ohos.sensor.agent.CategoryBodyAgent;
-import ohos.sensor.bean.CategoryBody;
-import ohos.sensor.data.CategoryBodyData;
-import ohos.sensor.listener.ICategoryBodyDataCallback;
-import ohos.utils.zson.ZSONObject;
+import ohos.agp.components.Component;
+import ohos.agp.components.Image;
 
 import java.nio.charset.StandardCharsets;
 
@@ -38,21 +28,24 @@ public class RemoteVideoPlayerAbilitySlice extends AbilitySlice {
     private Image buttonPause;
     private Image buttonRewind;
     private Image buttonFastForward;
+    private Image buttonPrevious;
+    private Image buttonNext;
 
-    private TaskDispatcher uiDispatcher;
     private P2pClient p2pClient;
 
-    private enum Command{
+    private enum Command {
         PLAY,
         PAUSE,
         FASTFORWARD,
-        REWIND
+        REWIND,
+        NEXT,
+        PREVIOUS
     }
 
     private final Receiver receiver = new Receiver() {
         @Override
         public void onReceiveMessage(HiWearMessage message) {
-            if (message.getType() == HiWearMessage.MESSAGE_TYPE_DATA){
+            if (message.getType() == HiWearMessage.MESSAGE_TYPE_DATA) {
                 final String text = new String(message.getData());
                 LogUtils.d(TAG, "Received text: " + text);
                 //TODO: parse player action
@@ -65,7 +58,6 @@ public class RemoteVideoPlayerAbilitySlice extends AbilitySlice {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_remotevideoplayer);
 
-        uiDispatcher = getUITaskDispatcher();
 
         initP2PClient();
         initViews();
@@ -89,6 +81,8 @@ public class RemoteVideoPlayerAbilitySlice extends AbilitySlice {
         buttonPause = (Image) findComponentById(ResourceTable.Id_buttonPause);
         buttonRewind = (Image) findComponentById(ResourceTable.Id_buttonRewind);
         buttonFastForward = (Image) findComponentById(ResourceTable.Id_buttonFastForward);
+        buttonPrevious = (Image) findComponentById(ResourceTable.Id_buttonPrevious);
+        buttonNext = (Image) findComponentById(ResourceTable.Id_buttonNext);
 
         buttonPlay.setClickedListener(component -> {
             sendPlayerCommand(Command.PLAY);
@@ -108,6 +102,14 @@ public class RemoteVideoPlayerAbilitySlice extends AbilitySlice {
 
         buttonRewind.setClickedListener(component -> {
             sendPlayerCommand(Command.REWIND);
+        });
+
+        buttonNext.setClickedListener(component -> {
+            sendPlayerCommand(Command.NEXT);
+        });
+
+        buttonPrevious.setClickedListener(component -> {
+            sendPlayerCommand(Command.PREVIOUS);
         });
     }
 
@@ -133,13 +135,27 @@ public class RemoteVideoPlayerAbilitySlice extends AbilitySlice {
         }
     }
 
-    private String commandToString(Command playerCommand){
-        String result="";
-        switch (playerCommand){
-            case PLAY:  result = "play"; break;
-            case PAUSE: result = "pause"; break;
-            case FASTFORWARD: result = "fastforward"; break;
-            case REWIND: result = "rewind"; break;
+    private String commandToString(Command playerCommand) {
+        String result = "";
+        switch (playerCommand) {
+            case PLAY:
+                result = "play";
+                break;
+            case PAUSE:
+                result = "pause";
+                break;
+            case FASTFORWARD:
+                result = "fastforward";
+                break;
+            case REWIND:
+                result = "rewind";
+                break;
+            case NEXT:
+                result = "next";
+                break;
+            case PREVIOUS:
+                result = "previous";
+                break;
         }
         return result;
     }

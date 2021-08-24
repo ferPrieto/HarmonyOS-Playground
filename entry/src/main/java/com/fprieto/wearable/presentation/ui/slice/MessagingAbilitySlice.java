639939d7901eb77c646a4f36f2bf71618292c6c2
ferPrieto <f.prieto.moyano@gmail.com>
2021-08-24 16:46:51 +0100

    Fixed image receiver

--- entry/src/main/java/com/fprieto/wearable/presentation/ui/slice/MessagingAbilitySlice.java
+++ entry/src/main/java/com/fprieto/wearable/presentation/ui/slice/MessagingAbilitySlice.java
@@ -1,21 +1,28 @@
 package com.fprieto.wearable.presentation.ui.slice;
 
 import com.fprieto.wearable.ResourceTable;
+import com.fprieto.wearable.model.DataMessage;
+import com.fprieto.wearable.model.PlayerCommand;
 import com.fprieto.wearable.presentation.ui.slice.joke.JokeAbilitySlice;
 import com.fprieto.wearable.util.LogUtils;
+import com.google.gson.Gson;
 import com.huawei.watch.kit.hiwear.HiWear;
 import com.huawei.watch.kit.hiwear.p2p.HiWearMessage;
 import com.huawei.watch.kit.hiwear.p2p.P2pClient;
 import com.huawei.watch.kit.hiwear.p2p.Receiver;
+import com.huawei.watch.kit.hiwear.p2p.SendCallback;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
-import ohos.agp.components.Button;
-import ohos.agp.components.ScrollView;
+import ohos.agp.components.*;
+import ohos.agp.components.Component.RotationEventListener;
 import ohos.app.dispatcher.TaskDispatcher;
 import ohos.media.image.ImageSource;
 import ohos.media.image.PixelMap;
+import ohos.multimodalinput.event.RotationEvent;
 
-public class MainAbilitySlice extends AbilitySlice {
+import java.nio.charset.StandardCharsets;
+
+public class MessagingAbilitySlice extends AbilitySlice {
 
     private final static String TAG = "MainAbilitySlice";
 
@@ -24,33 +31,43 @@ public class MainAbilitySlice extends AbilitySlice {
     private static final int FACTOR = 3;
     int rotationEventCount = 0;
 
+    private Image lastReceivedImage;
     private ScrollView scrollView;
+    private Text lastReceivedMessage;
 
     private TaskDispatcher uiDispatcher;
     private P2pClient p2pClient;
 
-    private final Receiver receiver = message -> {
-        final int type = message.getType();
+    private final Receiver receiver = new Receiver() {
+        @Override
+        public void onReceiveMessage(HiWearMessage message) {
+            final int type = message.getType();
 
-        switch (type) {
-            case HiWearMessage.MESSAGE_TYPE_DATA:
-                final String text = new String(message.getData());
-                LogUtils.d(TAG, "Received text: " + text);
-                break;
-            case HiWearMessage.MESSAGE_TYPE_FILE:
-                LogUtils.d(TAG, "Received file.");
-                final PixelMap pixelMap = ImageSource.create(message.getFile(),
-                        new ImageSource.SourceOptions())
-                        .createPixelmap(new ImageSource.DecodingOptions());
-                //todo: open messaging to show the image
-                break;
+            switch (type) {
+                case HiWearMessage.MESSAGE_TYPE_DATA:
+                    final String text = new String(message.getData());
+                    LogUtils.d(TAG, "Received text: " + text);
+                    uiDispatcher.syncDispatch(() -> {
+                        lastReceivedMessage.setText(text);
+                    });
+                    break;
+                case HiWearMessage.MESSAGE_TYPE_FILE:
+                    LogUtils.d(TAG, "Received file.");
+                    final PixelMap pixelMap = ImageSource.create(message.getFile(),
+                            new ImageSource.SourceOptions())
+                            .createPixelmap(new ImageSource.DecodingOptions());
+                    uiDispatcher.syncDispatch(() -> {
+                        lastReceivedImage.setPixelMap(pixelMap);
+                    });
+                    break;
+            }
         }
     };
 
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
-        super.setUIContent(ResourceTable.Layout_ability_main);
+        super.setUIContent(ResourceTable.Layout_ability_messaging);
 
         uiDispatcher = getUITaskDispatcher();
 
@@ -73,40 +90,12 @@ public class MainAbilitySlice extends AbilitySlice {
 
     private void initViews() {
         scrollView = (ScrollView) findComponentById(ResourceTable.Id_scrollview_main);
+        lastReceivedImage = (Image) findComponentById(ResourceTable.Id_image_received_image);
+        lastReceivedMessage = (Text) findComponentById(ResourceTable.Id_text_last_received_message);
 
-        final Button healthButton = (Button) findComponentById(ResourceTable.Id_button_health);
-        healthButton.setClickedListener(component -> {
-            present(new HealthAbilitySlice(), new Intent());
-        });
-
-        final Button messagingButton = (Button) findComponentById(ResourceTable.Id_button_messaging);
-        messagingButton.setClickedListener(component -> {
-            present(new MessagingAbilitySlice(), new Intent());
-        });
-
-        final Button recordButton = (Button) findComponentById(ResourceTable.Id_button_record_audio);
-        recordButton.setClickedListener(component -> {
-            present(new RecordAudioAbilitySlice(), new Intent());
-        });
-
-        final Button remotePlayerButton = (Button) findComponentById(ResourceTable.Id_button_play_remote_video);
-        remotePlayerButton.setClickedListener(component -> {
-            present(new RemoteVideoPlayerAbilitySlice(), new Intent());
-        });
-
-        final Button playAudioButton = (Button) findComponentById(ResourceTable.Id_button_play_audio);
-        playAudioButton.setClickedListener(component -> {
-            present(new AudioPlayerAbilitySlice(), new Intent());
-        });
-
-        final Button locationButton = (Button) findComponentById(ResourceTable.Id_button_location);
-        locationButton.setClickedListener(component -> {
-            present(new LocationAbilitySlice(), new Intent());
-        });
-
-        final Button jokeButton = (Button) findComponentById(ResourceTable.Id_button_joke);
-        jokeButton.setClickedListener(component -> {
-            present(new JokeAbilitySlice(), new Intent());
+        final Button sendMessageButton = (Button) findComponentById(ResourceTable.Id_button_send_message);
+        sendMessageButton.setClickedListener(component -> {
+            sendMessageToMobile("Hello from SmartWatch!");
         });
 
         scrollView.setReboundEffect(true);
@@ -128,6 +117,20 @@ public class MainAbilitySlice extends AbilitySlice {
             return false;
         });
         scrollView.setVibrationEffectEnabled(true);
+    }
+
+    private void sendMessageToMobile(String message) {
+        try {
+            HiWearMessage.Builder builder = new HiWearMessage.Builder();
+            builder.setPayload(message.getBytes(StandardCharsets.UTF_8));
+            HiWearMessage msgPayload = builder.build();
+            SendCallback sendCallback = i -> {
+                LogUtils.d(TAG, "Send result - " + i);
+            };
+            p2pClient.send(msgPayload, sendCallback);
+        } catch (Exception e) {
+            LogUtils.e(TAG, e.getMessage());
+        }
     }
 
     @Override
